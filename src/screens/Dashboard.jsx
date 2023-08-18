847a5948bedb3862f479e60f2b3cd8dbbe260aa0
dking1234 <39795989+dking1234@users.noreply.github.com>
2023-08-18 15:46:51 +0800

    all

---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,352 @@
+import React from "react";
+import "./style.css";
+
+export const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="div">
+        <div className="group">
+          <div className="text-wrapper">Kenneth Njau</div>
+        </div>
+        <div className="overlap">
+          <div className="text-wrapper-2">Analytics</div>
+          <div className="text-wrapper-3">Total Fuel Consumption</div>
+          <div className="text-wrapper-4">8.3 Billion</div>
+          <div className="text-wrapper-5">Monthly</div>
+          <div className="text-wrapper-6">Diesel</div>
+          <div className="text-wrapper-7">Petrol</div>
+          <div className="overlap-group">
+            <div className="rectangle" />
+          </div>
+          <div className="rectangle-wrapper">
+            <div className="rectangle-2" />
+          </div>
+          <div className="div-wrapper">
+            <div className="rectangle-3" />
+          </div>
+          <div className="overlap-2">
+            <div className="rectangle-4" />
+          </div>
+          <div className="overlap-3">
+            <div className="rectangle-5" />
+          </div>
+          <div className="overlap-4">
+            <div className="rectangle-6" />
+          </div>
+          <div className="overlap-5">
+            <div className="rectangle-7" />
+          </div>
+          <div className="overlap-6">
+            <div className="rectangle-8" />
+          </div>
+          <div className="overlap-7">
+            <div className="rectangle-9" />
+          </div>
+          <div className="overlap-8">
+            <div className="rectangle-10" />
+          </div>
+          <div className="overlap-9">
+            <div className="rectangle-11" />
+          </div>
+          <div className="overlap-10">
+            <div className="rectangle-12" />
+          </div>
+          <div className="overlap-11">
+            <div className="rectangle-13" />
+          </div>
+          <div className="overlap-12">
+            <div className="rectangle-14" />
+          </div>
+          <div className="overlap-13">
+            <div className="rectangle-15" />
+          </div>
+          <div className="overlap-14">
+            <div className="rectangle-16" />
+          </div>
+          <div className="overlap-15">
+            <div className="rectangle-9" />
+          </div>
+          <div className="overlap-16">
+            <div className="rectangle-6" />
+          </div>
+          <div className="overlap-17">
+            <div className="rectangle-17" />
+          </div>
+          <div className="overlap-18">
+            <div className="rectangle-18" />
+          </div>
+          <div className="overlap-19">
+            <div className="rectangle-19" />
+          </div>
+          <div className="overlap-20">
+            <div className="rectangle-20" />
+          </div>
+          <div className="overlap-21">
+            <div className="rectangle-21" />
+          </div>
+          <div className="overlap-22">
+            <div className="rectangle-2" />
+          </div>
+          <div className="text-wrapper-8">0</div>
+          <div className="text-wrapper-9">400 L</div>
+          <div className="text-wrapper-10">800 L</div>
+          <div className="text-wrapper-11">1200 L</div>
+          <div className="text-wrapper-12">1600 L</div>
+          <div className="text-wrapper-13">Jan</div>
+          <div className="text-wrapper-14">Feb</div>
+          <div className="text-wrapper-15">Mar</div>
+          <div className="text-wrapper-16">Apr</div>
+          <div className="text-wrapper-17">May</div>
+          <div className="text-wrapper-18">Jun</div>
+          <div className="text-wrapper-19">Jul</div>
+          <div className="text-wrapper-20">Aug</div>
+          <div className="text-wrapper-21">Sep</div>
+          <div className="text-wrapper-22">Oct</div>
+          <div className="text-wrapper-23">Nov</div>
+          <div className="text-wrapper-24">Dec</div>
+          <div className="rectangle-22" />
+          <div className="rectangle-23" />
+        </div>
+        <div className="overlap-23">
+          <div className="text-wrapper-25">Recent Activities</div>
+          <div className="upload-wrapper">
+            <img
+              className="upload"
+              alt="Upload"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/upload-3.png"
+            />
+          </div>
+          <div className="fuel-wrapper">
+            <img
+              className="fuel"
+              alt="Fuel"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/fuel-1.png"
+            />
+          </div>
+          <div className="img-wrapper">
+            <img
+              className="img"
+              alt="Fuel"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/fuel-1.png"
+            />
+          </div>
+          <div className="withdraw-wrapper">
+            <img
+              className="withdraw"
+              alt="Withdraw"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/withdraw.png"
+            />
+          </div>
+          <p className="ths">&nbsp;&nbsp;&nbsp;&nbsp;Ths. 1,250,000</p>
+          <p className="p">&nbsp;&nbsp;&nbsp;&nbsp; Ths. 150,000</p>
+          <p className="ths-2">&nbsp;&nbsp;&nbsp;&nbsp;Ths. 50,000</p>
+          <p className="ths-3">&nbsp;&nbsp;&nbsp;&nbsp; Ths. 250,000</p>
+          <p className="text-wrapper-26">20 April 2023, 08:20 AM</p>
+          <div className="text-wrapper-27">See all</div>
+          <p className="text-wrapper-28">20 April 2023, 08:20 AM</p>
+          <p className="text-wrapper-29">20 April 2023, 08:20 AM</p>
+          <p className="text-wrapper-30">20 April 2023, 08:20 AM</p>
+          <img
+            className="line"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-1.svg"
+          />
+          <img
+            className="line-2"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-1.svg"
+          />
+          <img
+            className="line-3"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-1.svg"
+          />
+          <img
+            className="line-4"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-1.svg"
+          />
+          <img
+            className="line-5"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-1.svg"
+          />
+          <p className="text-wrapper-31">Top up Via - Bank *892</p>
+          <p className="text-wrapper-32">Purchase 50 L by John Steven</p>
+          <p className="text-wrapper-33">Purchase 45 L by Omary Mzee</p>
+          <p className="text-wrapper-34">Request 70 L by Deo Mattew</p>
+        </div>
+        <div className="overlap-24">
+          <div className="text-wrapper-35">Total Balance</div>
+          <div className="text-wrapper-36">500 Liters of Fuel</div>
+          <h1 className="h-1">Ths. 100,500,000</h1>
+          <div className="overlap-25">
+            <div className="group-2" />
+            <div className="text-wrapper-37">Send fuel</div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <div className="text-wrapper-38">Buy fuel</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-26">
+          <div className="text-wrapper-39">Expenses</div>
+          <div className="text-wrapper-40">Monthly</div>
+          <div className="text-wrapper-41">Diesel</div>
+          <div className="text-wrapper-42">61.5%</div>
+          <div className="text-wrapper-43">38.5%</div>
+          <div className="text-wrapper-44">Petrol</div>
+          <div className="text-wrapper-45">Total Liters</div>
+          <div className="overlap-27">
+            <div className="ellipse" />
+            <div className="text-wrapper-46">Tsh.400,000,000</div>
+            <div className="text-wrapper-47">Total Expenses</div>
+            <div className="ellipse-2" />
+            <img
+              className="ellipse-3"
+              alt="Ellipse"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/ellipse-3.svg"
+            />
+          </div>
+          <div className="text-wrapper-48">Tsh.246,000,000</div>
+          <div className="text-wrapper-49">Tsh.154,000,000</div>
+          <div className="text-wrapper-50">124,262.193</div>
+          <img
+            className="line-6"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-7.svg"
+          />
+          <img
+            className="line-7"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-8.svg"
+          />
+          <img
+            className="line-8"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/line-9.svg"
+          />
+        </div>
+        <p className="text-wrapper-51">Welcome to CRDB Fuel Portal</p>
+        <div className="overlap-28">
+          <div className="text-wrapper-52">Logout</div>
+          <div className="group-3" />
+        </div>
+        <div className="overlap-29">
+          <div className="text-wrapper-53">Customize</div>
+          <div className="group-4" />
+        </div>
+        <div className="overlap-30">
+          <div className="text-wrapper-54">Set Fuel Price</div>
+          <div className="group-4" />
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-31">
+            <div className="text-wrapper-55">Main Menu</div>
+            <div className="overlap-group-3">
+              <div className="text-wrapper-56">Dashboard</div>
+              <img
+                className="home"
+                alt="Home"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/home@2x.png"
+              />
+            </div>
+            <div className="group-5">
+              <div className="text-wrapper-57">Help and support</div>
+              <img
+                className="img-2"
+                alt="Support"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/support@2x.png"
+              />
+            </div>
+            <img
+              className="crdb-bank-tanzania"
+              alt="Crdb bank tanzania"
+              src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/crdb-bank-tanzania-logo-7917f2cfab-seeklogo-2@2x.png"
+            />
+            <div className="group-6">
+              <div className="text-wrapper-58">Notification</div>
+              <img
+                className="img-2"
+                alt="Notification"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/notification@2x.png"
+              />
+            </div>
+            <div className="group-7">
+              <div className="text-wrapper-59">Transactions</div>
+              <img
+                className="img-2"
+                alt="Clipboard"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/clipboard@2x.png"
+              />
+            </div>
+            <div className="group-8">
+              <div className="text-wrapper-60">Reports</div>
+              <img
+                className="img-3"
+                alt="Statistics"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/statistics@2x.png"
+              />
+            </div>
+            <div className="group-9">
+              <div className="text-wrapper-61">Settings</div>
+              <img
+                className="img-2"
+                alt="Setting"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/setting@2x.png"
+              />
+            </div>
+            <div className="group-10">
+              <div className="text-wrapper-58">Drivers</div>
+              <img
+                className="img-2"
+                alt="Add user"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/add-user@2x.png"
+              />
+              <img
+                className="upload-2"
+                alt="Upload"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/upload-2@2x.png"
+              />
+            </div>
+            <div className="group-11">
+              <div className="text-wrapper-59">Fuel Providers</div>
+              <img
+                className="upload-2"
+                alt="Upload"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/upload-2@2x.png"
+              />
+              <img
+                className="img-2"
+                alt="Branding"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/branding@2x.png"
+              />
+            </div>
+            <div className="group-12">
+              <div className="text-wrapper-59">Branch</div>
+              <img
+                className="img-2"
+                alt="Agency"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/agency@2x.png"
+              />
+            </div>
+            <div className="group-13">
+              <div className="text-wrapper-62">Assets</div>
+              <img
+                className="upload-3"
+                alt="Upload"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/upload-2@2x.png"
+              />
+              <img
+                className="img-3"
+                alt="Digital asset"
+                src="https://generation-sessions.s3.amazonaws.com/67956cbf81aa7e5a8da7015c9e86251f/img/digital-asset@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
